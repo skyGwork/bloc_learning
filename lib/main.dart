64e06656058d964ffb5fc961e12501255a469536
skyGwork <skyGworkk@gmail.com>
2022-08-11 01:35:40 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -46,116 +46,79 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: BlocListener<CounterCubit, CounterState>(
-        listener: (context, state) {
-          if (state.wasIncremented == true) {
-            // ignore: deprecated_member_use
-            Scaffold.of(context).showSnackBar(const SnackBar(
-              content: Text('Incremented!'),
-              duration: Duration(milliseconds: 300),
-            ));
-          } else if (state.wasIncremented == false) {
-            // ignore: deprecated_member_use
-            Scaffold.of(context).showSnackBar(const SnackBar(
-              content: Text('Decremented!'),
-              duration: Duration(milliseconds: 300),
-            ));
-          }
-        },
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-
-              BlocBuilder<CounterCubit, CounterState>(
-                builder: (context, state) {
-                  if (state.counterValue < 0) {
-                    return CounterText(
-                      counterText: 'BRR, NEGATIVE ${state.counterValue}',
-                    );
-                  } else if (state.counterValue % 2 == 0) {
-                    return CounterText(
-                      counterText: 'YAAAY ${state.counterValue}',
-                    );
-                  } else if (state.counterValue == 5) {
-                    return const CounterText(
-                      counterText: 'HMM, NUMBER 5',
-                    );
-                  } else {
-                    return CounterText(
-                      counterText: state.counterValue.toString(),
-                    );
-                  }
-                },
-              ),
-
-              // BlocConsumer<CounterCubit, CounterState>(
-              //   listener: (context, state) {
-              //     if (state.wasIncremented == true) {
-              //       Scaffold.of(context).showSnackBar(
-              //         SnackBar(
-              //           content: Text('Incremented!'),
-              //           duration: Duration(milliseconds: 300),
-              //         ),
-              //       );
-              //     } else if (state.wasIncremented == false) {
-              //       Scaffold.of(context).showSnackBar(
-              //         const SnackBar(
-              //           content: Text('Decremented!'),
-              //           duration: Duration(milliseconds: 300),
-              //         ),
-              //       );
-              //     }
-              //   },
-              //   builder: (context, state) {
-              //     if (state.counterValue < 0) {
-              //       return Text(
-              //         'BRR, NEGATIVE ' + state.counterValue.toString(),
-              //         style: Theme.of(context).textTheme.headline4,
-              //       );
-              //     } else if (state.counterValue % 2 == 0) {
-              //       return Text(
-              //         'YAAAY ' + state.counterValue.toString(),
-              //         style: Theme.of(context).textTheme.headline4,
-              //       );
-              //     } else if (state.counterValue == 5) {
-              //       return Text(
-              //         'HMM, NUMBER 5',
-              //         style: Theme.of(context).textTheme.headline4,
-              //       );
-              //     } else
-              //       return Text(
-              //         state.counterValue.toString(),
-              //         style: Theme.of(context).textTheme.headline4,
-              //       );
-              //   },
-              // ),
-              const SizedBox(height: 24),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  FloatingActionButton(
-                    onPressed: () {
-                      BlocProvider.of<CounterCubit>(context).decrement();
-                    },
-                    tooltip: 'Decrement',
-                    child: const Icon(Icons.remove),
-                  ),
-                  FloatingActionButton(
-                    heroTag: Text('${widget.title} #2'),
-                    onPressed: () {
-                      BlocProvider.of<CounterCubit>(context).increment();
-                    },
-                    tooltip: 'Increment',
-                    child: const Icon(Icons.add),
-                  ),
-                ],
-              ),
-            ],
-          ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            BlocConsumer<CounterCubit, CounterState>(
+              listener: (context, state) {
+                if (state.wasIncremented == true) {
+                  // ignore: deprecated_member_use
+                  Scaffold.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Incremented!'),
+                      duration: Duration(milliseconds: 300),
+                    ),
+                  );
+                } else if (state.wasIncremented == false) {
+                  // ignore: deprecated_member_use
+                  Scaffold.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Decremented!'),
+                      duration: Duration(milliseconds: 300),
+                    ),
+                  );
+                }
+              },
+              builder: (context, state) {
+                if (state.counterValue < 0) {
+                  return Text(
+                    'BRR, NEGATIVE ${state.counterValue}',
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                } else if (state.counterValue % 2 == 0) {
+                  return Text(
+                    'YAAAY ${state.counterValue}',
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                } else if (state.counterValue == 5) {
+                  return Text(
+                    'HMM, NUMBER 5',
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                } else {
+                  return Text(
+                    state.counterValue.toString(),
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                }
+              },
+            ),
+            const SizedBox(height: 24),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).decrement();
+                  },
+                  tooltip: 'Decrement',
+                  child: const Icon(Icons.remove),
+                ),
+                FloatingActionButton(
+                  heroTag: Text('${widget.title} #2'),
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).increment();
+                  },
+                  tooltip: 'Increment',
+                  child: const Icon(Icons.add),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
