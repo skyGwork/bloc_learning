ca586ed8df810d20da2648c5e9dfeb4d608841f1
skyGwork <skyGworkk@gmail.com>
2022-08-12 17:46:59 +0530

    update

---
+++ lib/bloc/cubit/internet_cubit.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:bloc_learning/constants/enums.dart';
+
+part 'internet_state.dart';
+
+class InternetCubit extends Cubit<InternetState> {
+  final Connectivity connectivity;
+  StreamSubscription connectivityStreamSubscription;
+
+  InternetCubit({
+    required this.connectivity,
+    required this.connectivityStreamSubscription,
+  }) : super(InternetLoading()) {
+    monitorInternetConnection();
+  }
+
+  StreamSubscription<ConnectivityResult> monitorInternetConnection() {
+    return connectivityStreamSubscription =
+        connectivity.onConnectivityChanged.listen((connectivityResult) {
+      if (connectivityResult == ConnectivityResult.wifi) {
+        emitInternetConnected(ConnectionType.wifi);
+      } else if (connectivityResult == ConnectivityResult.mobile) {
+        emitInternetConnected(ConnectionType.mobile);
+      } else if (connectivityResult == ConnectivityResult.none) {
+        emitInternetDisconnected();
+      }
+    });
+  }
+
+  void emitInternetConnected(ConnectionType connectionType) =>
+      emit(InternetConnected(connectionType: connectionType));
+
+  void emitInternetDisconnected() => emit(InternetDisconnected());
+
+  @override
+  Future<void> close() {
+    connectivityStreamSubscription.cancel();
+    return super.close();
+  }
+}
